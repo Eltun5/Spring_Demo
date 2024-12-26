862a561cb83e6b3adf4cf439fa17988b9efe2964
Eltun <aslanoveltun28@gmail.com>
2024-12-26 11:35:25 +0400

    remove home.jsp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.util.Properties;
 import com.Spring_demo.config.AppConfig;
 
